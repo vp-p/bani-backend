5f9ed5aa01bc02cf0725786a3b3c24ef2a751959
vp-p <viniciuspenteadop@gmail.com>
2026-07-18 02:47:07 +0000

    Mapping ManyToMany relationships for item_topic and item_collection

---
+++ src/main/java/com/br/neo_curator/entities/Collection.java
@@ -0,0 +1,44 @@
+package com.br.neo_curator.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+
+@Entity
+@Table (name = "collection")
+
+public class Collection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "col_id")
+    private Long id;
+
+    @Column(name = "col_description")
+    private String description;
+
+    @ManyToMany(mappedBy = "collections")
+    private Set<Item> items;
+
+    public Collection() {
+    }
+
+    public Collection(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/test/java/com/br/neo_curator/NeoCuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.neo_curator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoCuratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

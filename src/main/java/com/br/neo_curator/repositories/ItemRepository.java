b0af77b37278c665e3f0c74365182bac28f52390
vp-p <viniciuspenteadop@gmail.com>
2026-07-20 21:44:25 +0000

    feat: Creating Item's repository interface

--- src/main/java/com/br/neo_curator/repositories/ItemRepository.java
+++ src/main/java/com/br/neo_curator/repositories/ItemRepository.java
@@ -1,5 +1,9 @@
 package com.br.neo_curator.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.neo_curator.entities.Item;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
     
 }

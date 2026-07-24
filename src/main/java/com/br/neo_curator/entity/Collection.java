6026eafc8e2ea012ce4210a08b2c6fdf446cf542
vp-p <viniciuspenteadop@gmail.com>
2026-07-24 01:10:16 +0000

    feat: creating 1-n relationship between users and collections

--- src/main/java/com/br/neo_curator/entity/Collection.java
+++ src/main/java/com/br/neo_curator/entity/Collection.java
@@ -5,10 +5,13 @@ import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,7 +30,17 @@ public class Collection {
     @ManyToMany(mappedBy = "collections")
     private Set<Item> items = new HashSet<>();
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "usr_id")
+    private NeoUser neoUser;
+
     public Collection() {
+    }
+
+    public Collection(String description, Set<Item> items, NeoUser neoUser) {
+        this.description = description;
+        this.items = items;
+        this.neoUser = neoUser;
     }
 
     public Collection(String description) {
@@ -41,4 +54,14 @@ public class Collection {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public NeoUser getNeoUser() {
+        return neoUser;
+    }
+
+    public void setNeoUser(NeoUser neoUser) {
+        this.neoUser = neoUser;
+    }
+
+    
 }
--- src/main/java/com/br/neo_curator/entity/NeoUser.java
+++ src/main/java/com/br/neo_curator/entity/NeoUser.java
@@ -1,12 +1,17 @@
 package com.br.neo_curator.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +30,10 @@ public class NeoUser {
     @Column(name = "usr_password")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)  
     private String password;
+
+    @OneToMany(mappedBy = "neoUser", fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Set<Collection> collections = new HashSet<>();
 
     public NeoUser() {
     }

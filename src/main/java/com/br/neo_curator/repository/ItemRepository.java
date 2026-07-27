12362ac456953d577ada3d0aa8f3a68168bf98b2
vp-p <viniciuspenteadop@gmail.com>
2026-07-27 23:00:27 +0000

    feat: Implementing some basic item's queries

--- src/main/java/com/br/neo_curator/repository/ItemRepository.java
+++ src/main/java/com/br/neo_curator/repository/ItemRepository.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.br.neo_curator.entity.Item;
+import com.br.neo_curator.entity.enums.ItemType;
 
 public interface ItemRepository extends IItemRepository {
     
@@ -12,5 +13,8 @@ public interface ItemRepository extends IItemRepository {
     public List<Item> findByTopicId(Long id);
     public List<Item> findByCollectionDescriptionContaining(String description);
     public List<Item> findByCollectionId(Long id);
-    
+    public List<Item> findTop20ByOrderByDateDesc();
+    public List<Item> findByType(ItemType type);
+    public List<Item> findBySourceKindContaining(String source);
+    public List<Item> findBySourceKindContainingAndType(String source, ItemType type);
 }

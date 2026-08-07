b52ec561527f9e2f7cca267081210d6552600bec
vp-p <viniciuspenteadop@gmail.com>
2026-08-07 01:17:30 +0000

    feat: Configuring login and register access.

--- src/main/java/com/br/neo_curator/infra/auth/SecurityConfigurations.java
+++ src/main/java/com/br/neo_curator/infra/auth/SecurityConfigurations.java
@@ -23,6 +23,8 @@ public class SecurityConfigurations {
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
             .requestMatchers(HttpMethod.POST, "/item").hasRole("ADMIN")
             .anyRequest().authenticated()
         )

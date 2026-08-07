2480728c4c436367eff71e5bfe68c7858245029a
vp-p <viniciuspenteadop@gmail.com>
2026-08-07 00:49:21 +0000

    .

--- src/main/java/com/br/neo_curator/infra/auth/SecurityConfigurations.java
+++ src/main/java/com/br/neo_curator/infra/auth/SecurityConfigurations.java
@@ -4,9 +4,13 @@ package com.br.neo_curator.infra.auth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +27,15 @@ public class SecurityConfigurations {
             .anyRequest().authenticated()
         )
         .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
